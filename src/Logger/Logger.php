187737a3f0b41c757eccea98c0c480be30b1211f
Stefan Hagspiel <info@solverat.com>
2023-08-09 14:18:54 +0200

    improve app logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -2,21 +2,14 @@
 
 namespace SeoBundle\Logger;
 
-use Pimcore\Bundle\ApplicationLoggerBundle\ApplicationLogger;
-
 class Logger implements LoggerInterface
 {
-    public function __construct(protected ApplicationLogger $applicationLogger)
+    public function __construct(protected \Psr\Log\LoggerInterface $logger)
     {
     }
 
     public function log(string $level, string $message, array $context = []): void
     {
-        $this->getLogger()->log($level, $message, $context);
-    }
-
-    protected function getLogger(): ApplicationLogger
-    {
-        return $this->applicationLogger::getInstance('seo-bundle', true);
+        $this->logger->log($level, $message, $context);
     }
 }
